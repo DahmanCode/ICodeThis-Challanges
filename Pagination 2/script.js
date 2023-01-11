819b742a5dbca0086f0bbae9999565c3a1b9b458
Dahman <94912743+DahmanCode@users.noreply.github.com>
2023-01-11 23:25:35 +0100

    'Pagination 2' Challenge

--- Pagination 2/script.js
+++ Pagination 2/script.js
@@ -22,3 +22,105 @@ $(document).ready(function() {
     $('.paginations').find('.pageNb.active').next().removeClass('active')
   })
 })
+
+// Another Way
+
+// const listy = document.getElementById('listy');
+// const prevBtn = document.getElementById('prev');
+// const nextBtn = document.getElementById('next');
+
+// const edgeNumbers = 3;
+// const numberOfItems = 14;
+// let currentActiveIdx = 8;
+// let prevIsDot = false;
+
+// for (let i = 0; i < numberOfItems; i++) {
+//     const li = document.createElement('li');
+
+//     if (i === currentActiveIdx) {
+//         li.classList.add('active');
+//     }
+
+//     li.innerText = i + 1;
+
+//     listy.appendChild(li);
+// }
+
+// const listEls = document.querySelectorAll('#listy li');
+
+// // initial call to create the dots
+// resetDots();
+
+// // add click event to all lis
+// listEls.forEach((el, idx) => {
+//     el.addEventListener('click', () => {
+//         currentActiveIdx = idx;
+//         setActive(el);
+//     });
+// });
+
+
+// prevBtn.addEventListener('click', () => {
+//     if (currentActiveIdx <= 0) {
+//         return;
+//     }
+
+//     currentActiveIdx--;
+
+//     setActive(listEls[currentActiveIdx]);
+// });
+
+// nextBtn.addEventListener('click', () => {
+//     if (currentActiveIdx >= listEls.length - 1) {
+//         return;
+//     }
+
+//     currentActiveIdx++;
+
+//     setActive(listEls[currentActiveIdx]);
+// });
+
+// function setActive(el) {
+//     removeActive();
+//     el.classList.add('active');
+
+//     if (currentActiveIdx === 0) {
+//         prevBtn.disabled = true;
+//     } else {
+//         prevBtn.disabled = false;
+//     }
+
+//     if (currentActiveIdx === numberOfItems - 1) {
+//         nextBtn.disabled = true;
+//     } else {
+//         nextBtn.disabled = false;
+//     }
+
+//     resetDots();
+// }
+
+// function removeActive() {
+//     listEls.forEach((el, idx) => {
+//         el.classList.remove('active')
+//     });
+// }
+
+// function resetDots() {
+//     console.log(currentActiveIdx);
+//     listEls.forEach((el, idx) => {
+        
+//         if (prevIsDot) {
+//             el.style.display = 'none';
+//             prevIsDot = false;
+//         } else {
+//             el.style.display = 'flex';
+//         }
+
+//         if (idx < edgeNumbers || idx > numberOfItems - (edgeNumbers + 2) || (idx > currentActiveIdx - 3 && idx < currentActiveIdx + 2)) {
+//             el.innerText = idx + 1;
+//         } else {
+//             el.innerText = '...';
+//             prevIsDot = true;
+//         }
+//     })
+// }
